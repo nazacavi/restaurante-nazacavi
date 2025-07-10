7dd9b50b47176d70a11017b114cd7131959298c3
NAZARET <nazacavi80@gamil.com>
2025-07-10 19:26:23 +0200

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+
+  return (
+    <>
+      <p>Estamos en la página inicial</p>
+      <br></br>
+      <a href="/pagina2">pagina 2</a>
+      <br></br>
+      <a href="/pagina3">pagina 3</a>
+    </>
+  );
+};
---
+++ src/Pagina2.tsx
@@ -0,0 +1,11 @@
+export default function Pagina2() {
+    return (
+        <>
+            <p>Estamos en la página 2</p>
+            <br></br>
+            <a href="/">inicio</a>
+            <br></br>
+            <a href="/pagina3">pagina 3</a>
+        </>
+    );
+}

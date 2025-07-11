8d6d78930446ab32c779c92ff2369b79abbb7316
NAZARET <nazacavi80@gamil.com>
2025-07-11 13:17:40 +0200

    Header y footer en todas las páginas

---
+++ src/components/Footer.tsx
@@ -0,0 +1,114 @@
+export default function Footer () {
+    return (
+        <>
+            <footer>
+
+
+
+                
+                <div className="logo-datos">   
+                <img
+                    className="imagen-logo"
+                    src="./imagenes/imagotipo.png"
+                    height="150"
+                    width="150"
+                    title="logo"
+                    alt="logo"
+                />
+                <button>inicio</button>
+                <button>menu</button>
+                <button>especialidades</button>
+                <button>carta</button>
+                <button>reservas</button>
+
+                </div>
+
+                <div className="redes-sociales">
+                <h2>Redes sociales</h2>
+
+                <a href="#"><i id="facebook" className="fa-brands fa-facebook">Síguenos en Facebook</i></a>
+            
+                <a href="#"><i id="instagram" className="fa-brands fa-instagram">Síguenos en Instagram</i></a>
+
+                <a href="#"><i id="linkedin" className="fa-brands fa-linkedin-in">Síguenos en Linkedin</i></a> 
+
+                <a href="#"><i className="fa-brands fa-pinterest">Síguenos en Pinterest</i></a>
+                </div>
+
+                <div className="informacion-contacto">
+                <h2>Información de contacto</h2>
+
+                <div>
+                    <i className="fa-solid fa-location-dot"></i>
+
+                    <address>Plaza del la Constitución 6, 00958, Madrid</address>
+                </div>
+
+                <div>
+                    <i className="fa-solid fa-mobile-screen-button"></i>
+
+                    <a href="tel:672125807">Móvil: 52412587</a>
+                </div>
+                
+                <div>
+                    <i className="fa-solid fa-envelope"></i>
+
+                    <a href="mailto:nacavi@hotmail.es">Email: nacavi@hotmail.es</a>
+                    
+                </div>
+
+                     
+                </div>
+
+                <table>
+                <thead>
+                    <tr>
+                        <th colSpan={2}>Horario de atención</th>
+                    </tr>
+
+                    <tr>
+                        <th>Día</th>
+                        <th>Horario</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Lunes</td>
+                        <td>Cerrado</td>
+                    </tr>
+
+                    <tr>
+                        <td>Martes a Jueves</td>
+                        <td>12:00 a 17:00</td>
+                    </tr>
+
+                    <tr>
+                        <td>Viernes</td>
+                        <td>12:00 a 17:00</td>
+                    </tr>
+
+                    <tr>
+                        <td>Sábado</td>
+                        <td>12:00 a 17:00</td>
+                    </tr>
+
+                     <tr>
+                        <td>Domingo</td>
+                        <td>12:00 a 17:00</td>
+                     </tr>
+                </tbody>
+                </table>
+
+                <div className="legal">
+                    <h2>Enlaces de interés</h2>
+                    <ul>
+                        <li><a href="#">Aviso legal</a></li>
+                        <li><a href="#">Política de privacidad</a></li>
+                        <li><a href="#">Política de cookies</a></li>
+                    </ul>
+                </div>
+            </footer>  
+        </>
+    )
+}

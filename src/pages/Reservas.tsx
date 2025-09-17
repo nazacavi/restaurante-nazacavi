f6bdcf0f96a413ed9af861caa66ccf2a0097e295
NAZARET <nazacavi80@gamil.com>
2025-09-17 18:20:22 +0200

    Modificado el formulario para que los inputs muestren errores

--- src/pages/Reservas.tsx
+++ src/pages/Reservas.tsx
@@ -5,32 +5,58 @@ import './Reservas.css';
 
 export default function Reservas() {
 
+    function borrarErrores() {
+        const $errores = document.getElementsByClassName('mensaje-error');
+        for(const $error of $errores) {
+            $error.remove();
+        }
+
+        const $inputs = document.getElementsByTagName('input');
+        for(const $input of $inputs) {
+            $input.style.border = '1px solid #979595';
+        }
+    }
+
     function enviarFormulario(event) {
+        borrarErrores();
+        let hayError = false;
         event.preventDefault();
 
         const nombre = document.getElementById('nombre').value
         const telefono = document.getElementById('telefono').value
 
         if(nombre === '') {
-            alert('El nombre no pude estar vacío');
-            return;
+            const $inputNombre = document.getElementById('nombre');
+            $inputNombre.style.border = '1px solid red';
+            $inputNombre.parentElement.innerHTML += '<span class="mensaje-error">El nombre no puede estar vacío</span>'
+            hayError = true;
         }
 
         if (nombre.includes('0') || nombre.includes('1') || nombre.includes('2') ||
             nombre.includes('3') || nombre.includes('4') || nombre.includes('5') ||
             nombre.includes('6') || nombre.includes('7') || nombre.includes('8') ||
             nombre.includes('9') || nombre.includes('.') || nombre.includes(','))  {
-            alert('El nombre no puede contener números, puntos ni comas');
-            return;
+            const $inputNombre = document.getElementById('nombre');
+            $inputNombre.style.border = '1px solid red';
+            $inputNombre.parentElement.innerHTML += '<span class="mensaje-error">El nombre no puede contener números, puntos ni comas</span>'
+            hayError = true;
         }
 
         if (telefono.includes(' ') || telefono.includes('.') || telefono.includes(',') ) {
-            alert('El teléfono debe contener solo numeros sin espacios y símbolos')
-            return;
+            const $inputNombre = document.getElementById('telefono');
+            $inputNombre.style.border = '1px solid red';
+            $inputNombre.parentElement.innerHTML += '<span class="mensaje-error">El teléfono debe contener solo numeros sin espacios y símbolos</span>'
+            hayError = true;
         }
 
         if (telefono.length < 9) {
-            alert('El teléfono debe tener al menos 9 dígitos');
+            const $inputNombre = document.getElementById('telefono');
+            $inputNombre.style.border = '1px solid red';
+            $inputNombre.parentElement.innerHTML += '<span class="mensaje-error">El teléfono debe tener al menos 9 dígitos</span>'
+            hayError = true;
+        }
+
+        if(hayError) {
             return;
         }
 
@@ -53,8 +79,6 @@ export default function Reservas() {
             <main>
                 <div className='formulario'>
                     <form
-                        method="post"
-                        action="#"
                         onSubmit={enviarFormulario}
                         id="datos"
                     >
@@ -62,7 +86,7 @@ export default function Reservas() {
 
                         <label>
                             <input
-                                id= "nombre"
+                                id="nombre"
                                 type="text"
                                 name="nombre"
                                 placeholder='nombre'
